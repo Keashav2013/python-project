578cdb147d3d71ebad3486ec3f4b7c8dbaedff13
Keashav2013 <keashav5403835@gmail.com>
2024-06-11 18:16:47 +0530

    Add files via upload

---
+++ project2.py
@@ -0,0 +1,6 @@
+# Password guessing
+Password = "Program"
+userpassword = input("Enter the password")
+while (Password!=userpassword):
+    userpassword =input("Enter the password")
+print("The password is correct you are allowed to login")
